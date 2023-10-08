0a4526d84419845b2cbd03ba991142f0845594d7
arpitsha28 <124351953+arpitsha28@users.noreply.github.com>
2023-10-08 19:23:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,9 @@
-import React ,{useState, useRef} from 'react'
-import '../styles/App.css';
-import PortalButton from './PortalButton.js';
-import PortalTextArea from './PortalTextArea.js';
-
-//complete the function 
 const App = () => {
-  const [inputText,setInputText] = useState('');
-
-  const handleBtnClick = (e)=>{
-    setInputText(e);
-  }
-
-
   return (
-    
-    <div id="main">
-      <input id="input" value={inputText} ></input><br/>
-      <PortalButton buttonclick={handleBtnClick} /><br/>
-      <PortalTextArea value={inputText}/>
+    <div id="root">
+      <input id="input" type="text" placeholder="Enter text" />
+      <PortalButton />
+      <PortalTextArea />
     </div>
-  )
-}
-
-
-export default App;
+  );
+};
