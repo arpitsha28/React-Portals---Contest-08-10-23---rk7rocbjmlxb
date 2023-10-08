69378e5817f2dcdb3ec9d772bcf9e38a1ec88c90
arpitsha28 <124351953+arpitsha28@users.noreply.github.com>
2023-10-08 18:31:40 +0530

    Update PortalButton.js

--- src/components/PortalButton.js
+++ src/components/PortalButton.js
@@ -2,14 +2,21 @@ import React from 'react';
 import  ReactDOM  from 'react-dom';
 
 //complete this function using portals
-const PortalButton=()=>{
-    return (
-        <div>
-            <button id="button" onClick={}>Click</button>
-        </div>
-        
-    )
-}
+const PortalButton = ({ onClick }) => {
+  const btnRef = useRef(null);
+
+  const handleClick = () => {
+    const text = document.getElementById("input").value;
+    onClick(text);
+  };
+  return ReactDOM.createPortal(
+    <button id="button" onClick={handleClick}>
+      Click
+    </button>,
+    btnRef.current
+  );
+};
 export default PortalButton;
+
 
 //portal-button id will be used here for portal purpose
