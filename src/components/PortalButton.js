a28bb1cdbb7d8030f0c54c58cf5cb0d388b46e87
arpitsha28 <124351953+arpitsha28@users.noreply.github.com>
2023-10-08 19:46:51 +0530

    Update PortalButton.js

--- src/components/PortalButton.js
+++ src/components/PortalButton.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const PortalButton = () => {
-  const handleClick = () => {
-    const inputText = document.getElementById('input').value;
-    const textarea = document.getElementById('textarea');
-    if (textarea) {
-      textarea.value = inputText;
-    }
-  };
-
+const PortalButton = ({ buttonClick }) => {
   return ReactDOM.createPortal(
-    <div id="portal-button">
-      <button id="button" onClick={handleClick}>Click</button>
+    <div>
+      <button id="button" onClick={buttonClick}>
+        Click
+      </button>
     </div>,
     document.getElementById('portal-button')
   );
