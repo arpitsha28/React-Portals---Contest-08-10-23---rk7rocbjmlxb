524c2dddeb831a22ed618424ceeff98b91a30f7a
arpitsha28 <124351953+arpitsha28@users.noreply.github.com>
2023-10-08 19:46:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState, useRef } from 'react';
 import '../styles/App.css';
 import PortalButton from './PortalButton';
 import PortalTextArea from './PortalTextArea';
 
 const App = () => {
+  const [inputValue, setInputValue] = useState('');
+  const textareaRef = useRef(null);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleButtonClick = () => {
+    if (textareaRef.current) {
+      textareaRef.current.value = inputValue;
+    }
+  };
+
   return (
-    <div id="root">
-      <input id="input" type="text" placeholder="Enter text" />
-      <PortalButton />
-      <PortalTextArea />
+    <div id="main">
+      <input id="input" value={inputValue} onChange={handleInputChange} />
+      <br />
+      <PortalButton buttonClick={handleButtonClick} />
+      <br />
+      <PortalTextArea textareaRef={textareaRef} />
     </div>
   );
 };
